4af04f62cd4ce8bc4ee46ea8cf15e33d61a05215
souleyFall <souleymanefall2002@gmail.com>
2026-04-20 09:06:54 +0200

    fix android studio

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  projectRoot: __dirname,
+  watchman: {
+    // Ignore Android build directories to prevent file watcher errors
+  },
+  resolver: {
+    blacklistRE: /android\/app\/\.cxx|android\/\.gradle|node_modules\/.*\.xcodeproj/,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
